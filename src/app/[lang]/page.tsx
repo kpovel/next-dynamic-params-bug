d87ba62c9213917adfae90f314b3c2a7dd16bde3
Pavlo Karas <kar.pavel123@gmail.com>
2024-02-15 12:28:50 +0200

    I need more cookies

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -1,7 +1,10 @@
+import { cookies } from "next/headers";
+
 export default function Page({ params }: { params: { lang: string } }) {
   if (params.lang !== "en" && params.lang !== "uk") {
     throw Error("unsupported language error");
   }
+  cookies().get("foo");
 
   return <h2>Current lang is {params.lang}</h2>;
 }
