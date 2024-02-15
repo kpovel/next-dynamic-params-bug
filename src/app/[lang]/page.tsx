47a3b2c213d70e9e52ff98c00ef878e4eac99cb0
Pavlo Karas <kar.pavel123@gmail.com>
2024-02-15 12:32:41 +0200

    use dynamic params in lang page

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -1,5 +1,7 @@
 import { cookies } from "next/headers";
 
+export const dynamicParams = false;
+
 export default function Page({ params }: { params: { lang: string } }) {
   if (params.lang !== "en" && params.lang !== "uk") {
     throw Error("unsupported language error");
